27a9a951e76aa257137747b66943f07a2ff1df89
team <zhaoyi@localhost.localdomain>
2020-08-16 21:06:08 +0800

    h5新闻详情页面布局

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Index from '@/components/Index/Index'
+import News from '@/components/Index/News'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +11,10 @@ export default new Router({
       path: '/',
       name: 'Index',
       component: Index
+    },{
+      path: '/news',
+      name: 'News',
+      component: News
     }
   ]
 })
